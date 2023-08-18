04d4d60fd095ebf18d20f63082605b4fe100055b
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-18 23:26:30 +0300

    Front <> Files Distributed & RRD Installed

--- recipe-front/src/App.js
+++ recipe-front/src/App.js
@@ -1,9 +1,15 @@
+import Authentication from './pages/Authentication';
 import './styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Authentication />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
