459f63b40f7af6d6c0ce174c3de2e38573c8c702
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:41:23 +0300

    Back <> Comment Model

--- recipe-server/app/Models/Comment.php
+++ recipe-server/app/Models/Comment.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = ['text', 'date', 'recipe_id'];
+
     public function recipe()
     {
         return $this->belongsTo(Recipe::class);
     }
-    }
+}
