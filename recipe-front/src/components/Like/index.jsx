4a384183a0791a858803816551a3959ab890aa2f
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:43:41 +0300

    Front <> Added Like

--- recipe-front/src/components/Like/index.jsx
+++ recipe-front/src/components/Like/index.jsx
@@ -2,114 +2,65 @@ import React, { useState, useEffect } from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import axios from 'axios';
 import './style.css';
+import { sendRequest } from "../../core/config/request";
+import { requestMethods } from "../../core/enums/requestMethods";
 
-const Like = ({ likes_count, post_id }) => {
-  const authtoken = localStorage.getItem('authtoken');
+const Like = ({ likes, liked, recipe_id }) => {
+  const [Liked, setLiked] = useState(liked);
+  const [numberOfLikes, setNumberOfLikes] = useState(likes);
 
-  const [like, setLike] = useState(false);
-  const [count, setCount] = useState(likes_count);
+  const handleLikeAxios = async () => {
+    const formData = new FormData();
+    formData.append('recipe_id', recipe_id);
+    try {
+      const response = await sendRequest({
+        method: "POST",
+        route: Liked ? "/unlike" : "like",
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      // Update the state after a successful response
+      setLiked(!Liked);
+      setNumberOfLikes(Liked ? numberOfLikes - 1 : numberOfLikes + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    checklikes();
-  }, []);
+    // You can also place the Axios call here if you want to use useEffect
+  }, [Liked]);
 
+  const handleLikeClick = () => {
+    // Update the state first
+    setLiked(!Liked);
+    setNumberOfLikes(Liked ? numberOfLikes - 1 : numberOfLikes + 1);
 
-  const checklikes = async () => {
-    let check = await checklike();
-    if (check == "already liked") {
-      setLike(true)
-    }
-
-
+    // Then make the Axios call
+    handleLikeAxios();
   }
-
-  const handleLikes = () => {
-    if (!like) {
-      getlikes();
-      setLike(true);
-      setCount(count + 1);
-    } else {
-      removelike();
-      setLike(false);
-      setCount(count - 1);
-    }
-  };
-
-  const getlikes = async () => {
-    try {
-      const response = await axios.post(
-        'http://127.0.0.1:8000/api/like',
-        { post_id },
-        {
-          headers: {
-            Authorization: `Bearer ${authtoken}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      return response.data.message
-    } catch (error) {
-      console.log('Error liking post:', error);
-    }
-  };
-
-
-  const checklike = async () => {
-    try {
-      const response = await axios.post(
-        'http://127.0.0.1:8000/api/checklike',
-        { post_id },
-        {
-          headers: {
-            Authorization: `Bearer ${authtoken}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      return response.data.message
-    } catch (error) {
-      console.log('Error liking post:', error);
-    }
-  };
-  const removelike = async () => {
-    try {
-      const response = await axios.post(
-        'http://127.0.0.1:8000/api/unlike',
-        { post_id },
-        {
-          headers: {
-            Authorization: `Bearer ${authtoken}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      return response.data.message
-    } catch (error) {
-      console.log('Error liking post:', error);
-    }
-  };
-
 
   return (
     <div className='like flex row'>
-
-      {like ? (
+      {Liked ? (
         <AiFillHeart
           size={30}
           className="text-danger"
-          onClick={handleLikes}
+          onClick={handleLikeClick}
           style={{ cursor: "pointer", color: "red" }}
         />
       ) : (
         <AiOutlineHeart
           size={30}
-          onClick={handleLikes}
+          onClick={handleLikeClick}
           style={{ cursor: "pointer" }}
         />
       )}
       <div className=''>
-        <p>{'5'}</p>
-
+        <p>{numberOfLikes}</p>
       </div>
     </div>
   );
