54060f887d50fa6f67bad13ce642e481fc8cc855
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:37:07 +0300

    Back <> Added Two Fields To Comments Table

--- recipe-server/database/migrations/2023_08_21_091120_create_comments_table.php
+++ recipe-server/database/migrations/2023_08_21_091120_create_comments_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('recipe_id');
+            $table->unsignedBigInteger('recipe_id');    
+            $table->text('text');
+            $table->date('date');
             $table->timestamps();
             $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
         });
