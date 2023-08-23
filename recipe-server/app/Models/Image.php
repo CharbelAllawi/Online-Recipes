adb7a0d932e35057d91ce0fb2371f8590252591a
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:41:33 +0300

    Back <> Image Model

--- recipe-server/app/Models/Image.php
+++ recipe-server/app/Models/Image.php
@@ -8,8 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $fillable = ['images'];
-
+    public function images()
+    {
+        return $this->hasMany(Image::class, 'recipe_id');
+    }
     public function recipe()
     {
+
         return $this->belongsTo(Recipe::class);
-    }}
+    }
+}
