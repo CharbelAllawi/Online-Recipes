8d506ea1c046de23f2833769f6c8af47b46a7ec2
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:38:39 +0300

    Back <> Recipe Controller

--- recipe-server/app/Http/Controllers/RecipeController.php
+++ recipe-server/app/Http/Controllers/RecipeController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Image;
 use App\Models\Ingredient;
 use App\Models\Recipe;
+use App\Models\RecipeLike;
+use App\Models\Shop;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
 
 class RecipeController extends Controller
 {
@@ -46,5 +47,73 @@ class RecipeController extends Controller
         $images->recipe_id = $recipeId;
         $images->images = $imageNames;
         $images->save();
+
+        $recipelike = new RecipeLike();
+        $recipelike->recipe_id = $recipeId;
+        $recipelike->save();
     }
-}
+
+        public function getRecipeInfo(Request $request)
+        {
+            $user = Auth::user();
+            $recipes = [];
+
+            // Check if the 'istrue' parameter is present and is true
+            if ($request->has('istrue') && $request->input('istrue')) {
+                // Check if the user is authenticated and has an associated ID
+                if ($user && $user->id) {
+                    $shoppingInfo = Shop::where('user_id', $user->id)
+                        ->pluck('recipe_id')
+                        ->toArray();
+
+                    $recipes = Recipe::with('images', 'ingredients', 'likes')
+                        ->whereIn('id', $shoppingInfo)
+                        ->get()
+                        ->map(function ($recipe) use ($user) {
+                            $imagesArray = explode(', ', $recipe->images->pluck('images')->first());
+                            $likesCount = $recipe->likes->count();
+                            $recipeData = [
+                                'recipe_id' => $recipe->id,
+                                'cuisine' => $recipe->cuisine,
+                                'images' => $imagesArray,
+                                'ingredients' => $recipe->ingredients,
+                                'likes' => $likesCount,
+                                'name' => $recipe->name,
+                                'liked' => false,
+                                'shopping_info' => true,
+                            ];
+
+                            if ($user) {
+                                $recipeData['liked'] = $recipe->likes->where('user_id', $user->id)->isNotEmpty();
+                            }
+
+                            return $recipeData;
+                        });
+                }
+            } else {
+                $recipes = Recipe::with('images', 'ingredients', 'likes')
+                    ->get()
+                    ->map(function ($recipe) use ($user) {
+                        $imagesArray = explode(', ', $recipe->images->pluck('images')->first());
+                        $likesCount = $recipe->likes->count();
+                        $recipeData = [
+                            'recipe_id' => $recipe->id,
+                            'cuisine' => $recipe->cuisine,
+                            'images' => $imagesArray,
+                            'ingredients' => $recipe->ingredients,
+                            'likes' => $likesCount,
+                            'name' => $recipe->name,
+                            'liked' => false,
+                        ];
+
+                        if ($user) {
+                            $recipeData['liked'] = $recipe->likes->where('user_id', $user->id)->isNotEmpty();
+                        }
+
+                        return $recipeData;
+                    });
+            }
+
+            return $recipes;
+        }
+    }
