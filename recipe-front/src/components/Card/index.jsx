78f1c3c1aff065cd46f62939787df387b70d0e80
CharbelAllawi <charbelallawiwork@gmail.com>
2023-08-23 12:44:34 +0300

    Front <> Fixed Card

--- recipe-front/src/components/Card/index.jsx
+++ recipe-front/src/components/Card/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './style.css';
 import Like from '../Like';
 import Comment from '../Comment';
+import shopingimg from '../../assets/shopping-cart.png';
+import shopingimgfill from '../../assets/shopping-cart-fill.png';
+import { sendRequest } from "../../core/config/request";
+import { requestMethods } from "../../core/enums/requestMethods";
 import {
   FacebookShareButton,
   WhatsappShareButton,
@@ -9,33 +13,77 @@ import {
   FacebookIcon,
   TwitterShareButton,
   TwitterIcon,
-  LinkedinShareButton,
-  LinkedinIcon,
   RedditShareButton,
-  RedditIcon
+  RedditIcon,
 } from 'react-share';
-const Card = ({ image_url = "french-food.jpg", likes_count, post_id }) => {
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight, faIcons, faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Import shopping cart icon
+import { FaShopware } from 'react-icons/fa';
+import * as FaIcons from 'react-icons/fa';
+
+const Card = ({ likes, liked, recipe_id, images, ingredients, name, cuisine }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handleAddToCart = async () => {
+
+    ingredients.forEach(async ingredient => {
+      const formData = new FormData();
+      formData.append('ingredients', ingredient.ingredients);
+      formData.append('recipe_id', ingredient.recipe_id);
+
+      try {
+
+        const response = await sendRequest({
+          method: "POST",
+          route: "/addShopping",
+          body: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        console.log(response)
+      } catch (error) {
+        console.log(error);
+      }
+      console.log(ingredients)
+    });
+  };
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   const shareUrl = 'https://www.pakkamarwadi.tk/';
 
   return (
     <>
       <li className="cards__item">
         <div className="card">
-          <img src={`http://localhost:8000/storage/images/posts/${image_url}`} alt="Image" />
+          {/* Image container */}
+          <div className="image-container">
+            <button className="arrow-button" onClick={previousImage}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <img
+              src={`http://localhost:8000/storage/images/posts/${images[currentImageIndex]}`}
+              alt="Image"
+            />
+            <button className="arrow-button" onClick={nextImage}>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
+
           <div className="card__content">
             <div className="card__title">
-
-
-
-
-
-
-
-
-
-
-              
-              <div className='socialmedia'>
+              <div className="socialmedia">
                 <FacebookShareButton
                   url={shareUrl}
                   quote={'Title or jo bhi aapko likhna ho'}
@@ -52,7 +100,6 @@ const Card = ({ image_url = "french-food.jpg", likes_count, post_id }) => {
                   <WhatsappIcon size={40} round={true} />
                 </WhatsappShareButton>
 
-
                 <TwitterShareButton
                   url={shareUrl}
                   quote={'Title or jo bhi aapko likhna ho'}
@@ -60,15 +107,6 @@ const Card = ({ image_url = "french-food.jpg", likes_count, post_id }) => {
                 >
                   <TwitterIcon size={40} round={true} />
                 </TwitterShareButton>
-
-                <LinkedinShareButton
-                  url={shareUrl}
-                  quote={'Title or jo bhi aapko likhna ho'}
-                  hashtag={'#portfolio...'}
-                >
-                  <LinkedinIcon size={40} round={true} />
-                </LinkedinShareButton>
-
                 <RedditShareButton
                   url={shareUrl}
                   quote={'Title or jo bhi aapko likhna ho'}
@@ -78,17 +116,25 @@ const Card = ({ image_url = "french-food.jpg", likes_count, post_id }) => {
                 </RedditShareButton>
               </div>
 
+              <Like recipe_id={recipe_id} likes={likes} liked={liked} />
+              <span className='shopimgdiv'>
+                <img
+                  className='shopimg'
+                  onClick={handleAddToCart}
+                  src={isHovered ? shopingimgfill : shopingimg}
+                  alt="Shopping Cart"
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                />
+              </span>
 
-              <Like likes_count={likes_count} post_id={post_id} />
+
             </div>
-            <Comment />
+            <Comment recipe_id={recipe_id} />
           </div>
-
         </div>
-
-      </li>
+      </li >
     </>
-
   );
 };
 
